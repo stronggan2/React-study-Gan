55d6b4187cbe40517d6ec52483d3168e156cdd9b
stronggan2 <118041708+stronggan2@users.noreply.github.com>
2022-12-05 21:01:39 +0900

    일기장

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./SimpleDiary/DiaryEditor.css";
 import DiaryEditor from "./SimpleDiary/DiaryEditor";
 import DiaryList from "./SimpleDiary/DiaryList";
-import LifeSycle from "./SimpleDiary/LifeSycle";
+
+//https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
 
+  // await 키워드랑 함께 이용하려고 promise를 반환하는 비동기 함수로 만들어줌
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime() + 1,
+        id: dataId.current++
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      getData();
+    }, 1500);
+  }, []);
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
@@ -39,9 +64,9 @@ function App() {
 
   return (
     <div className="App">
-      <LifeSycle/>
+      {/* <LifeSycle/> */}
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList onEdit= {onEdit} onRemove={onRemove} diaryList={data} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
