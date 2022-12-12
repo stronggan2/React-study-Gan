75ff1958d41e4fffc9be4b6393c5c3231b7aa65f
stronggan2 <118041708+stronggan2@users.noreply.github.com>
2022-12-12 18:52:28 +0900

    감성

--- src/EmotionDiary/pages/Diary.js
+++ src/EmotionDiary/pages/Diary.js
@@ -1,6 +1,9 @@
-import React from "react";
+import { useParams } from "react-router-dom";
 
 const Diary = () => {
+  const { id } = useParams();
+  console.log(id);
+
   return (
     <div>
       <h1>Diary</h1>
