d839684ca2ce9d7a27383319ea61abb56f182172
gan <kaeun181818@gmail.com>
2022-12-23 15:28:59 +0900

    ppack ㅁㅓㄱㅏ,,

--- src/coding_apple/ReactBlog.js
+++ src/coding_apple/ReactBlog.js
@@ -1,20 +1,51 @@
 import React from "react";
 import { useState } from "react";
-import "./ReactBlog.css"
+import "./ReactBlog.css";
+//Component : 많은 div들을 한 단어로 줄이고 싶으면
 
 const ReactBlog = () => {
   let posts = "강남 우동 맛집";
-  let [a,setA] = useState(["공덕 맛집", "크리스 마스 선물", "연말에 뭐하지?"]);
-
-
+  let [a, setA] = useState(["공덕 맛집", "크리스 마스 선물", "연말에 뭐하지?"]);
+  let [b, setB] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>React Blog</div>
       </div>
+
+      <button
+        onClick={() => {
+          let Scopy = [...a];
+          a.sort();
+          setA(Scopy);
+        }}
+      >
+        가나다순 정렬
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...a];
+          copy[0] = "공덕 술집";
+          setA(copy);
+        }}
+      >
+        {" "}
+        글수정
+      </button>
+
       <div className="list">
-        <h4>{a[0]}</h4>
+        <h4>
+          {a[0]}
+          <span
+            onClick={() => {
+              setB(b + 1);
+            }}
+          >
+            👍🏻
+          </span>{" "}
+          {b}
+        </h4>
         <p>12월 17일 발행</p>
       </div>
       <div className="list">
