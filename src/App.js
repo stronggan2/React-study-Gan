be201375a6c21e4b21ad28c662db5dd3aaa5e521
gan <kaeun181818@gmail.com>
2023-01-09 01:47:12 +0900

    쇼핑몰 기본틀

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
-// import Today from './Dday/Today';
-// import SetDday from './Dday/SetDday';
-import ReactBlog from './coding_apple/ReactBlog';
+import React from "react";
+import ReactBlog from "./coding_apple/ReactBlog";
+import "./coding_apple/ReactBlog.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Navbar, Container, Nav, Col, Row } from "react-bootstrap";
 
 const App = () => {
   return (
     <div>
-      <ReactBlog/>
-      {/* <Today/>
-      <SetDday/> */}
+      <ReactBlog />
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Gan-Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          <Col>
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+          <h4>상품명</h4>
+          <h4>상품설명</h4>
+          </Col>
+          <Col>
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+          <h4>상품명</h4>
+          <h4>상품설명</h4>
+          </Col>
+          <Col>
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+          <h4>상품명</h4>
+          <h4>상품설명</h4>
+          </Col> </Row>
+      </Container>
     </div>
   );
 };
--- src/coding_apple/ReactBlog.js
+++ src/coding_apple/ReactBlog.js
@@ -1,101 +1,11 @@
-import React from "react";
-import { useState } from "react";
-import "./ReactBlog.css";
-
-//props 자식이 부모 어쩌고~
+import React from 'react';
 
 const ReactBlog = () => {
-  let posts = "강남 우동 맛집";
-  let [a, setA] = useState(["공덕 맛집", "크리스 마스 선물", "연말에 뭐하지?"]);
-  let [b, setB] = useState([0, 0, 0]);
-  let [modal, setModal] = useState(false);
-
   return (
-    <div className="App">
-      <div className="black-nav">
-        <div>React Blog</div>
-      </div>
-
-      <button
-        onClick={() => {
-          let Scopy = [...a];
-          a.sort();
-          setA(Scopy);
-        }}
-      >
-        가나다순 정렬
-      </button>
-      {/* <button
-        onClick={() => {
-          let copy = [...a];
-          copy[0] = "공덕 술집";
-          setA(copy);
-        }}
-      >
-        {" "}
-        글수정
-      </button> */}
-
-      {/* <div className="list">
-        <h4>
-          {a[0]}
-          <span
-            onClick={() => {
-              setB(b + 1);
-            }}
-          >
-            👍🏻
-          </span>{" "}
-          {b}
-        </h4>
-        <p>12월 17일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4
-          onClick={() => {
-            setModal = true;
-          }}
-        >
-          {a[2]}
-        </h4>
-        {modal == true ? <Modal /> : null}
-
-        <p>12월 23일 발행</p> 
-      </div>
-        */}
-      {a.map(function (c, i) {
-        return (
-          <div className="list" key={i}> 
-            <h4 onClick={() => {setModal(true)}}>{a[i]}
-              {c}{" "}
-              <span
-                onClick={() => {
-                  let copy = [...b];
-                  copy[i] = copy[i] + 1;
-                  setB(copy);
-                }}
-              >
-                👍🏻
-              </span>
-              {b[i]}
-            </h4>
-            <p>12월 20일 발행</p>
-          </div>
-        );
-      })}
+    <div>
+      
     </div>
   );
 };
-
-function Modal() {
-  return (
-    <div className="Modal">
-      <h4>제목</h4>
-      <h4>날짜</h4>
-      <h4>상세내용</h4>
-    </div>
-  );
-}
 
 export default ReactBlog;
